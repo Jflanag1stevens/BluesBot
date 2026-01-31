af4d44c219c3282ba2c961f470734f676b22f59c
James Flanagan <jflanag1@stevens.edu>
2026-01-31 16:23:36 -0500

    HarmonicaPacket.c modification buffer implementation added, HarmonicaPacket.h initial commit

--- HarmonicaPacket.c
+++ HarmonicaPacket.c
@@ -1,5 +1,7 @@
 #include <stdint.h>
-#include <stdio.h>
+#include <stdlib.h>
+//#include <stdio.h>
+#include "HarmonicaPacket.h"
 
 #define TIMING_MASK 0xFFFFF
 #define TIMING_OFFSET 0
@@ -7,44 +9,41 @@
 #define SHUTTERS_OFFSET 21
 #define DIRECTION_MASK 0x1
 #define DIRECTION_OFFSET 31
-
-typedef struct {
+/*
+typedef struct{
     uint32_t time;
     uint16_t shutter;
     unsigned char direction;
 } harmonica_packet_t;
+*/
 
-uint32_t harmonica_packet_encoder(harmonica_packet_t* packet){
+
+uint32_t harmonica_packet_encoder(const harmonica_packet_t* packet){
     uint32_t returnPacket = 0;
     returnPacket = returnPacket | (packet->time & TIMING_MASK);
     returnPacket = returnPacket | (packet->shutter & SHUTTERS_MASK) << SHUTTERS_OFFSET;
     returnPacket = returnPacket | (packet->direction & DIRECTION_MASK) << DIRECTION_OFFSET;
     return returnPacket;
-
 }
 
+harmonica_packet_t* harmonica_packet_decode_buffer(size_t size, const uint32_t *array){
+    harmonica_packet_t *instruction_buffer = malloc(size * sizeof(harmonica_packet_t));
+    if(instruction_buffer == NULL){
+        return NULL;
+    }
+    for(size_t i = 0; i < size; i++){
+        harmonica_packet_decoder(array[i], &instruction_buffer[i]);
+    }
+    return instruction_buffer;
+}
 
-harmonica_packet_t* harmonica_packet_decoder(uint32_t packet){
-    harmonica_packet_t* instruction = (harmonica_packet_t*) malloc(sizeof(harmonica_packet_t));
+void harmonica_packet_decoder(const uint32_t packet, harmonica_packet_t *instruction){
     uint32_t timing = packet & TIMING_MASK;
     instruction->time = timing;
     uint16_t shutters = (packet >> SHUTTERS_OFFSET) & SHUTTERS_MASK;  
     instruction->shutter = shutters;
     unsigned char direction = (packet >> DIRECTION_OFFSET) & DIRECTION_MASK; 
     instruction->direction = direction;
-    return instruction;
 }
 
 
-int main(int argc, char **argv){
-printf("hello the size is %d", sizeof(harmonica_packet_t));
-uint32_t samplePacket = 0;
-uint32_t time = 249;
-samplePacket = samplePacket | time;
-uint16_t shutters = 0b1101101011;
-samplePacket = samplePacket | (shutters << 21);
-unsigned char direction = 1;
-samplePacket = samplePacket | (direction << 31);
-    harmonica_packet_t* packetPointer = harmonica_packet_construct(samplePacket);
-    printf("For %d millis we will do these shutters %d in this direction %d",packetPointer->time,packetPointer->shutter,packetPointer->direction);
-}
